b30568ba3b016cbfea561954067df5f315c42bd0
Suresh Kumar <your-email@example.com>
2026-05-05 01:31:15 +0530

    fix: remove netlify-cli and set node version for render

--- server/src/server.js
+++ server/src/server.js
@@ -86,8 +86,7 @@ app.use(compression());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "http://localhost:4173",
+     
       process.env.CLIENT_URL,
     ],
     credentials: true,
